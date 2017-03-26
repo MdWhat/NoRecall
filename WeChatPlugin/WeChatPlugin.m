ec99a86a107d145ffaad3f7cbcbe0924ad0e6ea0
CodeColorist <zhi.zhou@chaitin.com>
2017-03-26 22:49:12 +0800

    remove timeline, and prevent recalling message

--- WeChatPlugin/WeChatPlugin.m
+++ WeChatPlugin/WeChatPlugin.m
@@ -7,15 +7,15 @@
 //
 
 #import "WeChatPlugin.h"
-#import "MMTimeLineMainViewController.h"
-#import "MMTimeLineViewController.h"
+
+#import "PrivateHeaders/MessageService.h"
 
 #pragma mark - Plugin
 
 @implementation NSBundle (WeChatPlugin)
 
 + (instancetype)pluginBundle {
-    return [NSBundle bundleWithIdentifier:@"com.corbin.WeChatPlugin"];
+    return [NSBundle bundleWithIdentifier:@"cn.chaitin.WeChatPlugin"];
 }
 
 @end
@@ -26,35 +26,6 @@
 
 + (void)cb_logWithMMLogLevel:(int)arg1 module:(const char *)arg2 file:(const char *)arg3 line:(int)arg4 func:(const char *)arg5 message:(id)arg6 {
     NSLog(@"[%s] %s %s %@", arg2, arg3, arg5, arg6);
-}
-
-#pragma mark - MMCGIConfig
-
-- (const struct MMCGIItem *)cb_findItemWithFuncInternal:(int)arg1 {
-    struct MMCGIItem *res = (struct MMCGIItem *)[self cb_findItemWithFuncInternal:arg1];
-    if (arg1 == kMMCGIWrapTimeLineFunctionId) {
-        res = malloc(sizeof(struct MMCGIItem));
-        res->_field1 = kMMCGIWrapTimeLineFunctionId;
-        res->_field2 = 0;
-        res->_field3 = 0;
-        res->_field4 = "mmsnstimeline";
-        res->_field5 = CBGetClass(MMSnsTimeLineResponse);
-        res->_field6 = 1;
-        res->_field7 = 2;
-        res->_field8 = 0;
-    }
-    else if (arg1 == kMMCGIWrapHomePageFunctionId) {
-        res = malloc(sizeof(struct MMCGIItem));
-        res->_field1 = kMMCGIWrapHomePageFunctionId;
-        res->_field2 = 0;
-        res->_field3 = 0;
-        res->_field4 = "mmsnsuserpage";
-        res->_field5 = CBGetClass(SnsUserPageResponse);
-        res->_field6 = 1;
-        res->_field7 = 2;
-        res->_field8 = 0;
-    }
-    return res;
 }
 
 #pragma mark - AppDelegate
@@ -70,43 +41,27 @@
     return NSTerminateNow;
 }
 
-@end
+#pragma mark - MessageService
 
-@implementation NSView (WeChatPlugin)
+- (void)cb_onRevokeMsg:(id)arg {
+    NSLog(@"prevent message recall");
+}
 
 @end
 
 @implementation NSViewController (WeChatPlugin)
 
-#pragma mark - LeftViewController
-
-- (void)cb_setViewControllers:(NSArray *)vcs {
-    MMTimeLineMainViewController *timeLineMainVC = [[CBGetClass(MMTimeLineMainViewController) alloc] initWithNibName:@"MMContactsViewController" bundle:[NSBundle mainBundle]];
-    [timeLineMainVC setTitle:[[NSBundle mainBundle] localizedStringForKey:@"Tabbar.Chats" value:@"" table:0x0]];
-    MMTimeLineViewController *timeLineVC = [[CBGetClass(MMTimeLineViewController) alloc] initWithNibName:@"MMTimeLineViewController" bundle:[NSBundle pluginBundle]];
-    timeLineMainVC.detailViewController = (id)timeLineVC;
-    
-    MMTabbarItem *tabBarItem = [[CBGetClass(MMTabbarItem) alloc] initWithTitle:nil onStateImage:[[NSBundle pluginBundle] imageForResource:@"Tabbar-TimeLine-Selected"] onStateAlternateImage:[[NSBundle pluginBundle] imageForResource:@"Tabbar-TimeLine-Selected-HI"] offStateImage:[[NSBundle pluginBundle] imageForResource:@"Tabbar-TimeLine"] offStateAlternateImage:[[NSBundle pluginBundle] imageForResource:@"Tabbar-TimeLine-HI"]];
-    [timeLineMainVC setTabbarItem:tabBarItem];
-    
-    NSMutableArray *viewControllers = [vcs mutableCopy];
-    [viewControllers addObject:timeLineMainVC];
-    [self cb_setViewControllers:[viewControllers copy]];
-}
 
 @end
 
 static void __attribute__((constructor)) initialize(void) {
     NSLog(@"++++++++ WeChatPlugin loaded ++++++++");
     
-    CBRegisterClass(MMContactsViewController, MMTimeLineMainViewController);
-    
     CBHookClassMethod(MMLogger, @selector(logWithMMLogLevel:module:file:line:func:message:), @selector(cb_logWithMMLogLevel:module:file:line:func:message:));
     
-    CBHookInstanceMethod(MMCGIConfig, @selector(findItemWithFuncInternal:), @selector(cb_findItemWithFuncInternal:));
+    CBHookInstanceMethod(MessageService, @selector(onRevokeMsg:), @selector(cb_onRevokeMsg:));
     
     CBHookInstanceMethod(AppDelegate, @selector(applicationDidFinishLaunching:), @selector(cb_applicationDidFinishLaunching:));
     CBHookInstanceMethod(AppDelegate, @selector(applicationShouldTerminate:), @selector(cb_applicationShouldTerminate:));
     
-    CBHookInstanceMethod(LeftViewController, @selector(setViewControllers:), @selector(cb_setViewControllers:));
 }
